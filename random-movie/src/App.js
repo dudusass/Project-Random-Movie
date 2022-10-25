fa9bbd6da693100b01402a4246d593e1b8fba494
Sass <dudu_sass12@hotmail.com>
2022-10-25 05:16:24 -0300

    feat:page not found para engracadinhos e inicio da home

--- random-movie/src/App.js
+++ random-movie/src/App.js
@@ -1,14 +1,14 @@
-import { BrowserRouter, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
-import Home from './pages/Home'
-import NotFound from './pages/NotFound'
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Route exact path="/" component={ Home } />
-      <Route path="*" exact component={ NotFound } />
-    </BrowserRouter>
+    <Routes>
+      <Route exact path="/" element={ <Home /> } />
+      <Route path='*' element={ <NotFound /> } />
+    </Routes>
   );
 }
 
---
+++ random-movie/src/pages/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function NotFound() {
+  return (
+    <div>
+      <span class="display-1 d-block">404</span>
+      <div class="mb-4 lead">The page you are looking for was not found</div>
+      <a class="btn btn-link" href='/'>Back to Home</a>
+    </div>
+  )
+}
+
+export default NotFound;
